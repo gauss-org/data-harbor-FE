e9894ddec94a8742b91eb6ecc9611e5a67ab7bf4
wriddhi <wriddhihazra@gmail.com>
2024-11-13 19:26:27 +0530

    chore: [#8] installed tailwind-merge and clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { cn } from "@/lib";
 import Welcome from "./_components/Welcome";
 
 export default async function Home() {
   return (
-    <main>
+    <main className={cn("h-full min-h-screen w-full bg-slate-100/10 p-4")}>
       <Welcome />
     </main>
   );
