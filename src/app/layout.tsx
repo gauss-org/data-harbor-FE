08de8b97693f43134cbd7dedcf665b038dd2dba1
wriddhi <wriddhihazra@gmail.com>
2024-11-13 13:53:29 +0530

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Data Harbor",
+  description:
+    "Build your custom event schema and get started with our ingestion app",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
