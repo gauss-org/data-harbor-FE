d4e58295fd8f28a4de22a42a7d790c7fbef4b0c5
wriddhi <wriddhihazra@gmail.com>
2024-11-13 19:27:44 +0530

    chore: [#8] added classname utlity lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Welcome from "./_components/Welcome";
 
 export default async function Home() {
   return (
-    <main className={cn("h-full min-h-screen w-full bg-slate-100/10 p-4")}>
+    <main className={cn("h-full min-h-screen w-full", "bg-slate-100/10 p-4")}>
       <Welcome />
     </main>
   );
