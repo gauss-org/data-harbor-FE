86f5f63295a373218d72e1c2fd00733253af60cc
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-14 14:51:36 +0530

    chore: [#4] testing typechecker CI

--- src/app/_components/Welcome/index.tsx
+++ src/app/_components/Welcome/index.tsx
@@ -2,6 +2,8 @@ import { Text, Title, Center, Code } from "@mantine/core";
 import { Braces } from "lucide-react";
 
 export function Welcome() {
+  const test:string = 45;
+
   return (
     <main>
       <Center>
