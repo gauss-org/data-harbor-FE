f0489699962b9ac0a54f4655e555b608a0da59bb
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-14 16:44:21 +0530

    chore: [#4] test CI for prettier and eslint

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,9 +8,7 @@ Sentry.init({
   dsn: "https://0621e36d864026d7cf4645e421736647@o4508291740532736.ingest.de.sentry.io/4508291742761040",
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
