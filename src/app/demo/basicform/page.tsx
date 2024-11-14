f7eca02ba2e628ff6b578bf54d63f25b0c1b4575
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-14 19:19:20 +0530

    feat: [#10] add SurveyJS + create a demo page

---
+++ src/app/demo/basicform/page.tsx
@@ -0,0 +1,10 @@
+import dynamic from 'next/dynamic';
+const BasicFormComponent = dynamic(() => import("@/app/_components/demo/BasicForm"));
+
+export default function Survey() {
+  return (
+    <div className="flex min-h-screen flex-col items-center p-8">
+      <BasicFormComponent />
+    </div>
+  );
+}
