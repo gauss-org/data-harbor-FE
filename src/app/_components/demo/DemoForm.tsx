afc5d3e01332e5772182dc94d31843b150a6a122
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-15 18:20:57 +0530

    feat: [#10] testing theme switcher

--- src/app/_components/demo/DemoForm.tsx
+++ src/app/_components/demo/DemoForm.tsx
@@ -1,23 +1,76 @@
 "use client";
 
-import { Model } from "survey-core";
+import { type ITheme, Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.css";
-import { DefaultLight } from "survey-core/themes";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { DefaultLight, DefaultDark, DefaultLightPanelless, DefaultDarkPanelless, SharpLight, SharpDark, SharpLightPanelless, SharpDarkPanelless, BorderlessLight, BorderlessDark, BorderlessLightPanelless, BorderlessDarkPanelless, FlatLight, FlatDark, FlatLightPanelless, FlatDarkPanelless, PlainLight, PlainDark, PlainLightPanelless, PlainDarkPanelless, DoubleBorderLight, DoubleBorderDark, DoubleBorderLightPanelless, DoubleBorderDarkPanelless, LayeredLight, LayeredDark, LayeredLightPanelless, LayeredDarkPanelless, SolidLight, SolidDark, SolidLightPanelless, SolidDarkPanelless, ThreeDimensionalLight, ThreeDimensionalDark, ThreeDimensionalLightPanelless, ThreeDimensionalDarkPanelless, ContrastLight, ContrastDark, ContrastLightPanelless, ContrastDarkPanelless } from "survey-core/themes";
 import { json } from "data/demo/DemoFormJSON";
 import { DemoFormThemeSelector } from "./DemoFormThemeSelector";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+const themeMap: Record<string, ITheme> = {
+  DefaultLight,
+  DefaultDark,
+  DefaultLightPanelless,
+  DefaultDarkPanelless,
+  SharpLight,
+  SharpDark,
+  SharpLightPanelless,
+  SharpDarkPanelless,
+  BorderlessLight,
+  BorderlessDark,
+  BorderlessLightPanelless,
+  BorderlessDarkPanelless,
+  FlatLight,
+  FlatDark,
+  FlatLightPanelless,
+  FlatDarkPanelless,
+  PlainLight,
+  PlainDark,
+  PlainLightPanelless,
+  PlainDarkPanelless,
+  DoubleBorderLight,
+  DoubleBorderDark,
+  DoubleBorderLightPanelless,
+  DoubleBorderDarkPanelless,
+  LayeredLight,
+  LayeredDark,
+  LayeredLightPanelless,
+  LayeredDarkPanelless,
+  SolidLight,
+  SolidDark,
+  SolidLightPanelless,
+  SolidDarkPanelless,
+  ThreeDimensionalLight,
+  ThreeDimensionalDark,
+  ThreeDimensionalLightPanelless,
+  ThreeDimensionalDarkPanelless,
+  ContrastLight,
+  ContrastDark,
+  ContrastLightPanelless,
+  ContrastDarkPanelless
+};
 
 export default function DemoFormComponent() {
   const [selectedTheme, setSelectedTheme] = useState("DefaultLight");
-  const model = new Model(json);
-  model.applyTheme(DefaultLight);
+  
+  // initialize the model only once
+  const model = useMemo(() => {
+    const newModel = new Model(json);
+    newModel.applyTheme(themeMap.DefaultLight!);
+    return newModel;
+  }, []);
 
-  console.log(selectedTheme);
+  useEffect(()=>{
+    model.applyTheme(themeMap.selectedTheme!);
+    model.render();
+    console.log(selectedTheme);
+  }, [model, selectedTheme]);
 
   return (
     <>
-      <DemoFormThemeSelector setSelectedTheme={setSelectedTheme} />
+      <DemoFormThemeSelector setSelectedTheme={setSelectedTheme} key={selectedTheme} />
       <Survey model={model} />
     </>
   );
