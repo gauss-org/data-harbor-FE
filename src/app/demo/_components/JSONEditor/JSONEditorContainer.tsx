eaf4795cd742872dde22f68107ae1121cc56babb
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-18 13:38:18 +0530

    feat: [#10] add default schema to json editor

--- src/app/demo/_components/JSONEditor/JSONEditorContainer.tsx
+++ src/app/demo/_components/JSONEditor/JSONEditorContainer.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { JsonInput } from "@mantine/core";
+import { json } from "data/demo/DemoFormJSON";
+import { useState } from "react";
 
 export const JSONEditorContainer = () => {
+  const [value, setValue] = useState(JSON.stringify(json, null, 2));
 
   return (
     <div className="h-auto">
@@ -16,6 +19,8 @@ export const JSONEditorContainer = () => {
         minRows={4}
         formatOnBlur
         validationError="Invalid JSON"
+        defaultValue={value}
+        onChange={(val) => setValue(val)}
       />
     </div>
   );
