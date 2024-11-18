1390753b7cfc40a82a1c9520d2c60325160cd421
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2024-11-18 17:40:22 +0530

    feat: [#10] make the sections collapsable

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -4,6 +4,10 @@ import dynamic from "next/dynamic";
 import { JSONEditorContainer } from "@/app/demo/_components/JSONEditor/JSONEditorContainer";
 import { json } from "data/demo/DemoFormJSON";
 import { useState } from "react";
+import { Button, Collapse } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { cn } from "@/lib";
+
 const DemoFormContainer = dynamic(
   () => import("@/app/demo/_components/DemoForm/DemoFormContainer"),
 );
@@ -11,6 +15,8 @@ const DemoFormContainer = dynamic(
 export default function Survey() {
   const [inputSchema, setInputSchema] = useState(JSON.stringify(json, null, 2));
   const [isValidSchema, setIsValidSchema] = useState(true);
+  const [openedJSONEditor, handlersJSONEditor] = useDisclosure(false);
+  const [openedFormUI, handlersFormUI] = useDisclosure(false);
 
   return (
     <div className="flex min-h-screen flex-col items-center">
@@ -20,24 +26,40 @@ export default function Survey() {
       >
         <section
           id="JSON Editor"
-          className="w-1/3 rounded-md border-2 border-solid border-black bg-gray-200 p-6 shadow-md"
+          className={cn(
+            openedJSONEditor || !openedFormUI ? "w-full p-6" : "w-auto p-2",
+            "rounded-md border-2 border-solid border-black bg-gray-200 shadow-md",
+          )}
         >
-          <JSONEditorContainer
-            inputSchema={inputSchema}
-            setInputSchema={setInputSchema}
-            isValidSchema={isValidSchema}
-            setIsValidSchema={setIsValidSchema}
-          />
+          <Button onClick={handlersJSONEditor.toggle} className={cn(!openedJSONEditor && "mt-6")}>
+            {openedJSONEditor ? "<" : ">"}
+          </Button>
+          <Collapse in={openedJSONEditor}>
+            <JSONEditorContainer
+              inputSchema={inputSchema}
+              setInputSchema={setInputSchema}
+              isValidSchema={isValidSchema}
+              setIsValidSchema={setIsValidSchema}
+            />
+          </Collapse>
         </section>
         <section
           id="FORM UI"
-          className="w-2/3 rounded-md border-2 border-solid border-black bg-gray-200 p-6 shadow-md"
+          className={cn(
+            openedFormUI || !openedJSONEditor ? "w-full p-6" : "w-auto p-2",
+            "rounded-md border-2 border-solid border-black bg-gray-200 shadow-md",
+          )}
         >
-          <DemoFormContainer
-            isValidSchema={isValidSchema}
-            inputSchema={inputSchema}
-            setInputSchema={setInputSchema}
-          />
+          <Button onClick={handlersFormUI.toggle} className={cn(!openedFormUI && "mt-6")}>
+            {openedFormUI ? ">" : "<"}
+          </Button>
+          <Collapse in={openedFormUI}>
+            <DemoFormContainer
+              isValidSchema={isValidSchema}
+              inputSchema={inputSchema}
+              setInputSchema={setInputSchema}
+            />
+          </Collapse>
         </section>
       </section>
     </div>
